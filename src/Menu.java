fde3251103a29896caf1c124071ed93111f09655
HugooPaixao <hugopaixao079@gmail.com>
2026-09-02 09:59:42 -0300

    feat: build a "body" for my project

---
+++ src/Menu.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    Scanner scanner = new Scanner(System.in);
+    String option;
+
+    public void menu() {
+
+        switch (option) {
+
+            case "add":
+                break;
+            case "update":
+                break;
+            case "mark-in-progress":
+                break;
+            case "mark-done":
+                break;
+            case "list":
+                break;
+            }
+    }
+}
---
+++ src/Task.java
@@ -0,0 +1,54 @@
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private int id;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void toJson(String json) {
+
+    }
+}
